cbad514e829a52c312e969e39678d99e0fece76d
ngvrtech <ngvrtech@gmail.com>
2024-02-02 22:59:35 -0500

    Field Inventory completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import FieldHome from "./components/FieldHome";
 import FieldOptions from "./components/FieldOptions";
+import FieldInventory from "./components/FieldInventory";
 import "./scss/styles.scss";
 // import * as bootstrap from "bootstrap";
 
@@ -16,6 +17,7 @@ function App() {
             <Route path="/" element={<Login />} />
             <Route path="/field/" element={<FieldHome />} />
             <Route path="/field/:id" element={<FieldOptions />} />
+            <Route path="/field/:id/inventory" element={<FieldInventory />} />
           </Routes>
         </BrowserRouter>
       </div>
