fbc7ccd46ee1a49975aa7a7e53b4f7308131804e
Henry Kim <hnrykm@gmail.com>
2024-06-26 19:58:37 -0400

    Login component completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./components/Login";
-// import "./App.css";
 import "./scss/styles.scss";
 // import * as bootstrap from "bootstrap";
 
