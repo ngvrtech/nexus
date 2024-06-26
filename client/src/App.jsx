9d8f0914bb7c4f9569125a37d53993cee5b9aec8
Henry Kim <hnrykm@gmail.com>
2024-06-26 19:58:37 -0400

    field home completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./components/Login";
+import FieldHome from "./components/FieldHome";
 import "./scss/styles.scss";
 // import * as bootstrap from "bootstrap";
 
@@ -12,6 +13,7 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Login />} />
+            <Route path="/field/home" element={<FieldHome />} />
           </Routes>
         </BrowserRouter>
       </div>
